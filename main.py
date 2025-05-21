10b35c96635061094d628acc82e605a80ecca67d
Daria Supalova <dasupalova@gmail.com>
2025-05-21 18:50:20 +0300

    Add files via upload
    v2.0

--- main.py
+++ main.py
@@ -22,21 +22,20 @@ class Player(GameSprite):
     def update_r(self):
        keys = key.get_pressed()
        if keys[K_UP] and self.rect.y > 5:
-           self.rect.y += self.speed
-       if keys[K_DOWN] and self.rect.y < 495:
            self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < 495:
+           self.rect.y += self.speed
     def update_l(self):
        keys = key.get_pressed()
        if keys[K_w] and self.rect.y > 5:
-           self.rect.y += self.speed
-       if keys[K_s] and self.rect.y < 495:
            self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < 495:
+           self.rect.y += self.speed
 
 win_width = 700
 win_height = 500
 
-display.set_caption("Shooter")
-#background = transform.scale(image.load(img_back), (win_width, win_height))
+display.set_caption("Ping-pong")
 
 back = (200, 255, 255) 
 win_width = 600
@@ -51,6 +50,15 @@ racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
+speed_x = 3
+speed_y = 3
+
+font1 = font.SysFont('Arial', 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+
+font2 = font.SysFont('Arial', 35)
+lose2 = font2.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
 while game:
     for e in event.get():
        if e.type == QUIT:
@@ -62,9 +70,26 @@ while game:
         racket1.reset()
         racket2.update_r()
         racket2.reset()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y > (win_height - 50) or ball.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200,200))
+
+        if ball.rect.y > win_width:
+            finish = True
+            window.blit(lose2, (200,200))
+
         ball.reset()
 
         display.update()
 
-    time.delay(50)
-
+    clock.tick(50)
